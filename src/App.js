a1286fc8ce326f0cd12af4311cd860b15ef6b362
LarryWho11 <lara.pollehn@googlemail.com>
2020-03-18 12:22:05 +0100

    skeleton

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './css/styles.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
